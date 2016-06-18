7fead619979458a63c6c3d3664f27a18898dd488
Jorge Vargas <jvargas2@icloud.com>
2016-06-18 17:21:27 -0400

    initial commit

---
+++ StringGeneticAlgorithm/Individual.swift
@@ -0,0 +1,99 @@
+//
+//  Individual.swift
+//  HexCodeGeneticAlgorithm
+//
+//  Created by Jorge Vargas on 5/22/16.
+//  Copyright © 2016 Jorge Vargas. All rights reserved.
+//
+
+import UIKit
+
+class Individual {
+    
+    //MARK: Properties
+    var value = ""
+    var fitnessScore: Float!
+    private var target: String!
+    
+    init(targetString: String) {
+        self.target = targetString
+        for _ in 0...(targetString.characters.count - 1) {
+            let newGeneInt = Int(arc4random_uniform(95)) + 32
+            let newGeneChar = Character(UnicodeScalar(newGeneInt))
+            self.value.append(newGeneChar)
+        }
+        self.fitnessScore = self.calculateFitnessScore()
+    }
+    
+    init(value: String, targetString: String) {
+        self.value = value
+        self.target = targetString
+        self.fitnessScore = self.calculateFitnessScore()
+    }
+    
+    func breedWith(otherInd: Individual) -> Individual {
+        var newValue = ""
+        let selfArray = Array(self.value.characters)
+        let otherArray = Array(otherInd.value.characters)
+        let targetArray = Array(self.target.characters)
+        
+        for (i, char) in selfArray.enumerate() {
+            let s = Int(char.utf8Value())
+            let o = Int(otherArray[i].utf8Value())
+            let t = Int(targetArray[i].utf8Value())
+            let betterInt = abs(s - t) < abs(o - t) ? s : o
+            newValue.append(Character(UnicodeScalar(betterInt)))
+        }
+        return Individual(value: newValue, targetString: self.target)
+    }
+    
+    func mutate() {
+        self.value = ""
+        
+        for _ in 0...(self.target.characters.count - 1) {
+            let newGeneInt = Int(arc4random_uniform(95)) + 32
+            let newGeneChar = Character(UnicodeScalar(newGeneInt))
+            self.value.append(newGeneChar)
+        }
+        
+        //        let mIndex = Int(arc4random_uniform(UInt32(self.target.characters.count - 1)))
+        //        var valueArray = Array(self.value.characters)
+        //        let newGeneInt = Int(arc4random_uniform(95)) + 32
+        //        let newGeneChar = Character(UnicodeScalar(newGeneInt))
+        //        valueArray[mIndex] = newGeneChar
+        //        self.value = String(valueArray)
+        
+        self.fitnessScore = self.calculateFitnessScore()
+    }
+    
+    private func calculateFitnessScore() -> Float {
+        let selfArray = Array(self.value.characters)
+        let targetArray = Array(self.target.characters)
+        var totalValue = 0
+        for (i, char) in selfArray.enumerate() {
+            let s = Int(char.utf8Value())
+            let t = Int(targetArray[i].utf8Value())
+            totalValue += abs(s - t)
+        }
+        return Float(totalValue) / Float(selfArray.count)
+    }
+}
+
+// Adapted from http://stackoverflow.com/questions/24102044/how-can-i-get-the-unicode-code-points-of-a-character
+extension String {
+    func unicodeScalarCodePoint() -> UInt32 {
+        let scalars = self.unicodeScalars
+        return scalars[scalars.startIndex].value
+    }
+}
+
+// Adapted from http://stackoverflow.com/questions/24463992/converting-character-in-an-array-to-an-integer
+extension Character {
+    func utf8Value() -> UInt8 {
+        for s in String(self).utf8 {
+            return s
+        }
+        return 0
+    }
+}
+
---
+++ StringGeneticAlgorithm/ViewController.swift
@@ -0,0 +1,143 @@
+//
+//  ViewController.swift
+//  HexCodeGeneticAlgorithm
+//
+//  Created by Jorge Vargas on 5/22/16.
+//  Copyright © 2016 Jorge Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var targetTextField: UITextField!
+    @IBOutlet weak var runButton: UIButton!
+    @IBOutlet weak var currentGenerationTextLabel: UILabel!
+    @IBOutlet weak var bestIndividualTextLabel: UILabel!
+    @IBOutlet weak var generationSlider: UISlider!
+    
+    private var currentGenerationIndex = 0
+    private var generations = [Population]()
+    private var solutionFound = false
+    
+    // MARK: Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.targetTextField.text = "Genetic algorithms in Swift"
+        
+        runGeneticAlgorithm()
+    }
+    
+    func updateView() {
+        let bestIndividual = generations[currentGenerationIndex].bestIndividual
+        self.currentGenerationTextLabel.text = "Generation " + String(self.currentGenerationIndex)
+        self.bestIndividualTextLabel.text = bestIndividual.value
+    }
+    
+    func runGeneticAlgorithm() {
+        self.solutionFound = false
+        self.generations = [Population]()
+        self.currentGenerationIndex = 0
+        self.generationSlider.value = Float(self.currentGenerationIndex)
+        self.generations.append(Population(targetValue: self.targetTextField.text))
+        
+        while self.solutionFound == false {
+            self.generations.append(self.generations.last!.breedNewGeneration())
+            
+            if self.generations.last!.bestIndividual.value == self.targetTextField.text {
+                self.solutionFound = true
+            } else if self.generations.count > 500 {
+                self.solutionFound = true
+            }
+        }
+        
+        self.generationSlider.maximumValue = Float(generations.count - 1)
+        self.updateView()
+        
+        //        for (i, gen) in self.generations.enumerate() {
+        //            print("Generation \(i)")
+        //
+        //            for ind in gen.individuals {
+        //                print("\(ind.value), \(ind.fitnessScore)")
+        //            }
+        //        }
+    }
+    
+    @IBAction func runButtonPressed(sender: UIButton) {
+        runGeneticAlgorithm()
+    }
+    
+    @IBAction func sliderValueChanged(sender: UISlider) {
+        self.currentGenerationIndex = Int(sender.value)
+        self.updateView()
+    }
+}
+
+
+// MARK: Structs
+struct Population {
+    var individuals = [Individual]()
+    var bestIndividual: Individual!
+    
+    init(targetValue: String!) {
+        for i in 0...19 {
+            let newIndividual = Individual(targetString: targetValue)
+            self.individuals.append(newIndividual)
+            if i == 0 {
+                self.bestIndividual = newIndividual
+            } else if newIndividual.fitnessScore < bestIndividual.fitnessScore {
+                self.bestIndividual = newIndividual
+            }
+        }
+    }
+    
+    init(individuals: Array<Individual>) {
+        self.individuals = individuals
+        self.bestIndividual = self.individuals[0]
+        for ind in self.individuals {
+            if ind.fitnessScore < self.bestIndividual.fitnessScore {
+                self.bestIndividual = ind
+            }
+        }
+    }
+    
+    func breedNewGeneration() -> Population {
+        var newIndividuals = [Individual]()
+        var remainingIndividuals = self.individuals
+        
+        for _ in 0...((self.individuals.count / 2) - 1) {
+            let firstIndex = Int(arc4random_uniform(UInt32(remainingIndividuals.count - 1)))
+            let firstIndividual = remainingIndividuals[firstIndex]
+            remainingIndividuals.removeAtIndex(firstIndex)
+            let secondIndex = Int(arc4random_uniform(UInt32(remainingIndividuals.count - 1)))
+            let secondIndividual = remainingIndividuals[secondIndex]
+            var fitterInd: Individual
+            
+            if firstIndividual.fitnessScore < secondIndividual.fitnessScore {
+                fitterInd = firstIndividual
+            } else if secondIndividual.fitnessScore < firstIndividual.fitnessScore {
+                fitterInd = secondIndividual
+            } else {
+                let randomBool = arc4random_uniform(2) == 0 ? true : false
+                if randomBool {
+                    fitterInd = firstIndividual
+                } else {
+                    fitterInd = secondIndividual
+                }
+            }
+            
+            newIndividuals.append(fitterInd)
+            let newInd = firstIndividual.breedWith(secondIndividual)
+            if (Int(arc4random_uniform(10)) > 7) {
+                newInd.mutate()
+            }
+            newIndividuals.append(newInd)
+        }
+        
+        return Population(individuals: newIndividuals)
+    }
+}
+
+
